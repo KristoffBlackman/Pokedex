2a91b58ad85516dcb7a74d89c0bdb00aef609198
Kristoff Blackman <73307819+KristoffBlackman@users.noreply.github.com>
2021-01-20 19:07:26 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+//changed unregister() to ()register so app will work offline and load faster
+serviceWorker.register();
